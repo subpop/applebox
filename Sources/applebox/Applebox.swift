38a6c22f0b8f2b1a3d66c592b3eb543dbcdda306
Link Dupont <link@sub-pop.net>
2026-03-11 14:15:40 -0400

    Update version to 0.1.1

--- Sources/applebox/Applebox.swift
+++ Sources/applebox/Applebox.swift
@@ -37,7 +37,7 @@ struct Applebox: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "box",
         abstract: "Persistent Linux development containers for macOS",
-        version: "0.1.0",
+        version: "0.1.1",
         subcommands: [
             Create.self,
             Enter.self,
